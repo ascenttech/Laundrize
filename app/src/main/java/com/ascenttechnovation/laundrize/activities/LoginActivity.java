ff3af050c4a4031506792a479cb577366d1de66b
sagar Devanga <sagardevanga@gmail.com>
2015-06-29 13:27:10 +0530

    Added class

---
+++ app/src/main/java/com/ascenttechnovation/laundrize/activities/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.ascenttechnovation.laundrize.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ascenttechnovation.laundrize.R;
+
+/**
+ * Created by ADMIN on 29-06-2015.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
